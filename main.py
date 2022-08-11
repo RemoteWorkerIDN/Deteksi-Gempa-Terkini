e4bc2b7273525458512f9b0a7fb8ac389ab30bfb
RemoteWorkerIDN <danahiswara.danas@gmail.com>
2022-08-11 12:34:14 +0700

    Projek Deteksi Gempa Terkini_update v 0.0.7

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gempaterkirni
 
 if __name__ == "__main__" :
-    print("Aplikasi Utama")
+    print(f"Deskripsi aplikasi utama: {gempaterkirni.description}")
     result = gempaterkirni.ekstraksi_data()
     gempaterkirni.tampilkan_data(result)
